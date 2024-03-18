06254fed5dc3dada3801871a6ce1b34fd18e0c7d
Ahmet Atmaca <atmacasoftware52@gmail.com>
2024-03-18 11:02:15 +0300

    try-catch

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,36 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        //Defining variable
-        int indis;
 
-        //Defining array
-        int[] arr = {1, 87, 99, 100, 150, 40, 23, 33, 44, 60};
+    //Defining array
+    static int[] arr = {1, 87, 99, 100, 150, 40, 23, 33, 44, 60};
 
-        //Calling Scanner class
-        Scanner input = new Scanner(System.in);
-
+    static void getValueWithIndex(int index) {
         //Process try-catch
         try {
-            System.out.print("İndis numarasını giriniz : ");
-            indis = input.nextInt();
-
-            int getValue = arr[indis];
-            System.out.println("Dizinin " + indis + "'inci elemanı : " + getValue);
-
-        } catch (InputMismatchException e) {
-            System.out.println("Int türünde değer girmeniz gerekmektedir.");
+            int getValue = arr[index];
+            System.out.println("Dizinin " + index + "'inci elemanı : " + getValue);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Dizi 10 boyutlu olup boyut dışında değer girdiniz. Lütfen 0 - 9 arasında değer giriniz !");
             System.out.println(e.getMessage());
         }
+    }
 
+    public static void main(String[] args) {
+        //Defining variable
+        int indis;
+
+        //Calling Scanner class
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İndis numarasını giriniz : ");
+
+        try {
+            indis = input.nextInt();
+            getValueWithIndex(indis);
+        } catch (InputMismatchException e) {
+            System.out.println("Int türünde değer girmeniz gerekmektedir.");
+        }
 
     }
 }
